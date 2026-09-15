b5423c3285e2f70fd8a97ec482025f7f9bcc7ebc
Isha <ishavaghamshi@gmail.com>
2026-09-15 18:14:10 +0530

    Implement multiplication of two numbers program

---
+++ 01_variables_datatypes/program_3.py
@@ -0,0 +1,7 @@
+# WAP to input 2 floating point numbers & print their average.
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+average = (a + b) / 2
+print(average)
